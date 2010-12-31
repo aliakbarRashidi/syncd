07903f9b25f32e606e645451574bb57e946c05f8
Robin Burchell <robin.burchell@collabora.co.uk>
2010-12-31 05:51:32 +0000

    Fix UDP broadcasts to work.

--- src/saesucloudstorageadvertiser.cpp
+++ src/saesucloudstorageadvertiser.cpp
@@ -1,5 +1,6 @@
 // Qt
 #include <QCoreApplication>
+#include <QNetworkInterface>
 
 // Saesu
 #include "sglobal.h"
@@ -11,10 +12,7 @@
 SaesuCloudStorageAdvertiser::SaesuCloudStorageAdvertiser(QObject *parent)
     : QObject(parent)
 {
-    connect(&mBroadcaster, SIGNAL(readyRead()), SLOT(onReadyRead()));
-    connect(&mServer, SIGNAL(newConnection()), SLOT(onNewConnection()));
-
-    if (!mBroadcaster.bind(QHostAddress::Any, 1337)) {
+    if (!mBroadcaster.bind(QHostAddress::Broadcast, 1337)) {
         qDebug("Couldn't bind UDP; presuming another instance running");
         exit(1);
     }
@@ -23,6 +21,13 @@ SaesuCloudStorageAdvertiser::SaesuCloudStorageAdvertiser(QObject *parent)
         qDebug("Couldn't bind TCP; presuming another instance running");
         exit(1);
     }
+
+    // let the world know that we are ready for business
+    mBroadcaster.writeDatagram("HELLO", 5, QHostAddress::Broadcast, 1337);
+
+    // and open for business
+    connect(&mBroadcaster, SIGNAL(readyRead()), SLOT(onReadyRead()));
+    connect(&mServer, SIGNAL(newConnection()), SLOT(onNewConnection()));
 }
 
 void SaesuCloudStorageAdvertiser::onReadyRead()
@@ -33,7 +38,17 @@ void SaesuCloudStorageAdvertiser::onReadyRead()
 
     int returnedSize = mBroadcaster.readDatagram(datagram.data(), datagram.size(),
                                                  &senderAddress, &senderPort);
-    if (returnedSize == 5) {
+
+    QNetworkInterface iface;
+
+    foreach (const QHostAddress &addr, iface.allAddresses()) {
+        if (senderAddress == addr) {
+            sDebug() << "Dropping broadcast from myself (" << senderAddress << ")";
+            return;
+        }
+    }
+
+    if (returnedSize == 5 && datagram == "HELLO") {
         // Great joy
         qDebug() << senderAddress << " said " << datagram << ", connecting back...";
         SaesuCloudStorageSynchroniser *syncSocket = new SaesuCloudStorageSynchroniser(this);
