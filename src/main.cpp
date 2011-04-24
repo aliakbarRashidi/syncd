19e5ebbd52e5523f68df970362909ce414f29900
Robin Burchell <viroteck@viroteck.net>
2011-04-24 03:07:22 +0100

    Set organisation info.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@ int main(int argc, char **argv)
 {
     QCoreApplication a(argc, argv);
 
+    a.setOrganizationName(QLatin1String("saesu"));
+    a.setApplicationName(QLatin1String("syncd"));
+
     SaesuCloudStorageAdvertiser storageAdvertiser;
     a.exec();
 }
