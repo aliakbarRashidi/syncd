7a8bd1622f3fb6130e5d9fde0afa772b0b2c97ee
Robin Burchell <viroteck@viroteck.net>
2011-07-27 16:48:10 +0200

    Prepare for public releasing.

--- src/syncadvertiser.cpp
+++ src/syncadvertiser.cpp
@@ -1,12 +1,17 @@
 /*
- * Copyright (C) 2011 Robin Burchell <viroteck@viroteck.net>
+ * Copyright (C) 2010-2011 Robin Burchell
  *
- * This software, and all associated material(s), including but not limited
- * to documentation are protected by copyright. All rights reserved.
- * Copying, including reproducing, storing, adapting, translating, or
- * reverse-engineering any or all of this material requires prior written
- * consent. This material also contains confidential information which
- * may not be disclosed in any form without prior written consent.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 // Qt
